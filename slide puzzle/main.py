39033299285a2fcdf01dd0366ce2dab8d7545588
mohamed-sofiane-belgacem <114389556+mohamed-sofiane-belgacem@users.noreply.github.com>
2023-02-27 15:37:31 +0100

    Add files via upload

---
+++ slide puzzle/main.py
@@ -0,0 +1,270 @@
+import pygame
+import random
+import time
+
+# COULEURS (r, g, b)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+BGCOLOUR = DARKGREY
+
+# PARAMETRE DU JEUX
+WIDTH = 800
+HEIGHT = 641
+FPS = 60
+title = "Sliding Puzzle Game"
+TILESIZE = 128
+GAME_SIZE = 3
+
+import pygame
+
+pygame.font.init()
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, game, x, y, text):
+        self.groups = game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pygame.Surface((TILESIZE, TILESIZE))
+        self.x, self.y = x, y
+        self.text = text
+        self.rect = self.image.get_rect()
+        if self.text != "empty":
+            self.font = pygame.font.SysFont("Consolas", 50)
+            font_surface = self.font.render(self.text, True, BLACK)
+            self.image.fill(WHITE)
+            self.font_size = self.font.size(self.text)
+            draw_x = (TILESIZE / 2) - self.font_size[0] / 2
+            draw_y = (TILESIZE / 2) - self.font_size[1] / 2
+            self.image.blit(font_surface, (draw_x, draw_y))
+        else:
+            self.image.fill(BGCOLOUR)
+
+    def update(self):
+        self.rect.x = self.x * TILESIZE
+        self.rect.y = self.y * TILESIZE
+
+    def click(self, mouse_x, mouse_y):
+        return self.rect.left <= mouse_x <= self.rect.right and self.rect.top <= mouse_y <= self.rect.bottom
+
+    def right(self):
+        return self.rect.x + TILESIZE < GAME_SIZE * TILESIZE
+
+    def left(self):
+        return self.rect.x - TILESIZE >= 0
+
+    def up(self):
+        return self.rect.y - TILESIZE >= 0
+
+    def down(self):
+        return self.rect.y + TILESIZE < GAME_SIZE * TILESIZE
+
+
+class UIElement:
+    def __init__(self, x, y, text):
+        self.x, self.y = x, y
+        self.text = text
+
+    def draw(self, screen):
+        font = pygame.font.SysFont("Consolas", 30)
+        text = font.render(self.text, True, WHITE)
+        screen.blit(text, (self.x, self.y))
+
+
+class Button:
+    def __init__(self, x, y, width, height, text, colour, text_colour):
+        self.colour, self.text_colour = colour, text_colour
+        self.width, self.height = width, height
+        self.x, self.y = x, y
+        self.text = text
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.colour, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont("Consolas", 30)
+        text = font.render(self.text, True, self.text_colour)
+        self.font_size = font.size(self.text)
+        draw_x = self.x + (self.width / 2) - self.font_size[0] / 2
+        draw_y = self.y + (self.height / 2) - self.font_size[1] / 2
+        screen.blit(text, (draw_x, draw_y))
+
+    def click(self, mouse_x, mouse_y):
+        return self.x <= mouse_x <= self.x + self.width and self.y <= mouse_y <= self.y + self.height
+
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(title)
+        self.clock = pygame.time.Clock()
+        self.melanger_time = 0
+        self.start_melanger = False
+        self.previous_choice = ""
+        self.start_game = False
+        self.start_timer = False
+        self.elapsed_time = 0
+
+
+    def save_score(self):
+        with open("high_score.txt", "w") as file:
+            file.write(str("%.3f\n" % self.high_score))
+
+    def create_game(self):
+        grid = [[x + y * GAME_SIZE for x in range(1, GAME_SIZE + 1)] for y in range(GAME_SIZE)]
+        grid[-1][-1] = 0
+        return grid
+
+    def melanger(self):
+        possible_moves = []
+        for row, tiles in enumerate(self.tiles):
+            for col, tile in enumerate(tiles):
+                if tile.text == "empty":
+                    if tile.right():
+                        possible_moves.append("right")
+                    if tile.left():
+                        possible_moves.append("left")
+                    if tile.up():
+                        possible_moves.append("up")
+                    if tile.down():
+                        possible_moves.append("down")
+                    break
+            if len(possible_moves) > 0:
+                break
+
+        if self.previous_choice == "right":
+            possible_moves.remove("left") if "left" in possible_moves else possible_moves
+        elif self.previous_choice == "left":
+            possible_moves.remove("right") if "right" in possible_moves else possible_moves
+        elif self.previous_choice == "up":
+            possible_moves.remove("down") if "down" in possible_moves else possible_moves
+        elif self.previous_choice == "down":
+            possible_moves.remove("up") if "up" in possible_moves else possible_moves
+
+        choice = random.choice(possible_moves)
+        self.previous_choice = choice
+        if choice == "right":
+            self.tiles_grid[row][col], self.tiles_grid[row][col + 1] = self.tiles_grid[row][col + 1], \
+                                                                       self.tiles_grid[row][col]
+        elif choice == "left":
+            self.tiles_grid[row][col], self.tiles_grid[row][col - 1] = self.tiles_grid[row][col - 1], \
+                                                                       self.tiles_grid[row][col]
+        elif choice == "up":
+            self.tiles_grid[row][col], self.tiles_grid[row - 1][col] = self.tiles_grid[row - 1][col], \
+                                                                       self.tiles_grid[row][col]
+        elif choice == "down":
+            self.tiles_grid[row][col], self.tiles_grid[row + 1][col] = self.tiles_grid[row + 1][col], \
+                                                                       self.tiles_grid[row][col]
+
+    def draw_tiles(self):
+        self.tiles = []
+        for row, x in enumerate(self.tiles_grid):
+            self.tiles.append([])
+            for col, tile in enumerate(x):
+                if tile != 0:
+                    self.tiles[row].append(Tile(self, col, row, str(tile)))
+                else:
+                    self.tiles[row].append(Tile(self, col, row, "empty"))
+
+    def new(self):
+        self.all_sprites = pygame.sprite.Group()
+        self.tiles_grid = self.create_game()
+        self.tiles_grid_completed = self.create_game()
+        self.elapsed_time = 0
+        self.start_timer = False
+        self.start_game = False
+        self.buttons_list = []
+        self.buttons_list.append(Button(500, 100, 200, 50, "melanger", WHITE, BLACK))
+        self.buttons_list.append(Button(500, 170, 200, 50, "Reset", WHITE, BLACK))
+        self.draw_tiles()
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        if self.start_game:
+            if self.tiles_grid == self.tiles_grid_completed:
+                self.start_game = False
+                if self.high_score > 0:
+                    self.high_score = self.elapsed_time if self.elapsed_time < self.high_score else self.high_score
+                else:
+                    self.high_score = self.elapsed_time
+                self.save_score()
+
+            if self.start_timer:
+                self.timer = time.time()
+                self.start_timer = False
+            self.elapsed_time = time.time() - self.timer
+
+        if self.start_melanger:
+            self.melanger()
+            self.draw_tiles()
+            self.melanger_time += 1
+            if self.melanger_time > 120:
+                self.start_melanger = False
+                self.start_game = True
+                self.start_timer = True
+
+        self.all_sprites.update()
+
+    def draw_grid(self):
+        for row in range(-1, GAME_SIZE * TILESIZE, TILESIZE):
+            pygame.draw.line(self.screen, LIGHTGREY, (row, 0), (row, GAME_SIZE * TILESIZE))
+        for col in range(-1, GAME_SIZE * TILESIZE, TILESIZE):
+            pygame.draw.line(self.screen, LIGHTGREY, (0, col), (GAME_SIZE * TILESIZE, col))
+
+    def draw(self):
+        self.screen.fill(BGCOLOUR)
+        self.all_sprites.draw(self.screen)
+        self.draw_grid()
+        for button in self.buttons_list:
+            button.draw(self.screen)
+        UIElement(550, 35, "%.3f" % self.elapsed_time).draw(self.screen)
+        pygame.display.flip()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit(0)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                for row, tiles in enumerate(self.tiles):
+                    for col, tile in enumerate(tiles):
+                        if tile.click(mouse_x, mouse_y):
+                            if tile.right() and self.tiles_grid[row][col + 1] == 0:
+                                self.tiles_grid[row][col], self.tiles_grid[row][col + 1] = self.tiles_grid[row][col + 1], self.tiles_grid[row][col]
+
+                            if tile.left() and self.tiles_grid[row][col - 1] == 0:
+                                self.tiles_grid[row][col], self.tiles_grid[row][col - 1] = self.tiles_grid[row][col - 1], self.tiles_grid[row][col]
+
+                            if tile.up() and self.tiles_grid[row - 1][col] == 0:
+                                self.tiles_grid[row][col], self.tiles_grid[row - 1][col] = self.tiles_grid[row - 1][col], self.tiles_grid[row][col]
+
+                            if tile.down() and self.tiles_grid[row + 1][col] == 0:
+                                self.tiles_grid[row][col], self.tiles_grid[row + 1][col] = self.tiles_grid[row + 1][col], self.tiles_grid[row][col]
+
+                            self.draw_tiles()
+
+                for button in self.buttons_list:
+                    if button.click(mouse_x, mouse_y):
+                        if button.text == "melanger":
+                            self.melanger_time = 0
+                            self.start_melanger = True
+                        if button.text == "Reset":
+                            self.new()
+
+
+game = Game()
+while True:
+    game.new()
+    game.run()
+use_y <= self.y + self.height
